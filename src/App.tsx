765d95f7b7ffe4afb317cb24ddd3d2b84b03a052
Philip <philipmjobe@gmail.com>
2023-03-31 14:34:52 -0400

    creates campgrounds component and puts it into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,13 @@
 import React from 'react';
 // import './App.css';
-// import { useState } from 'react';
-import axios from 'axios';
+import Campgrounds from "./components/campgrounds.tsx"
 
-interface Campground {
-  id: number;
-  lon: number;
-  lat: number;
-  gps_composite_field: string;
-  campground_code: string;
-  campground_name: string;
-  campground_type: string;
-  phone_number: string;
-  dates_open: string;
-  comments: string;
-  number_of_campsites: number;
-  elevation: string;
-  amenities: string;
-  state: string;
-  nearest_town: string;
-}
-
-const defaultCampgrounds: Campground[] = [];
-
-const App = () => {
-  const [campgrounds, setCampgrounds]: [Campground[], (campgrounds: Campground[]) => void] = React.useState(
-    defaultCampgrounds
-    );
-  const [loading, setLoading]: [
-    boolean,
-    (loading: boolean) => void
-  ] = React.useState<boolean>(true)
-
-  const [error, setError]: [string, (error: string) => void]= React.useState('')
-
-  React.useEffect(() => {
-    axios.get<Campground[]>("http://localhost:3000/campgrounds", {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(response => {
-      setCampgrounds(response.data);
-      setLoading(false)
-    })
-    .catch(ex => {
-      const error = ex.response.status === 404 ? "Resource Not Found" : "An Unexpected Error Has Occurred"
-      setError(error)
-      setLoading(false)
-    })
-  }, []);
-
+function App() {
   return (
-    <div className='App'>
-      <ul className='campgrounds'>
-        {campgrounds.map((campground) => (
-          <li key={campground.id}>
-            <h3>{campground.campground_name}</h3>
-          </li>
-        ))}
-      </ul>
-      {error && <p className='error'>{error}</p>}
+    <div className="App">
+     <Campgrounds />
     </div>
-  )
+  );
 }
-
-
 
 export default App;
