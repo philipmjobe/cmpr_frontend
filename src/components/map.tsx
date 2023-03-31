34e6c9c710ca2f96ae509d868e36a9dd999f1f16
Philip <philipmjobe@gmail.com>
2023-03-31 15:41:59 -0400

    moves .env to gitignore

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicGhpbGlwam9iZSIsImEiOiJjbGZ3dWhsdXcwYmJ0M2VxY2tyZjgzc3lkIn0.8BkhvRQElOx7NXiGvRBA-A';
+mapboxgl.accessToken = '.env';
 
 export default function App() {
   const mapContainer = useRef(null);
