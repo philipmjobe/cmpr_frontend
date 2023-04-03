c282490d1726942f2f8ec0d042964f19160ea5f2
Philip <philipmjobe@gmail.com>
2023-04-03 13:10:44 -0400

    changed starting lon and lat to use the geographical center of north america

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,13 +1,13 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = '.env';
+mapboxgl.accessToken = 'pk.eyJ1IjoicGhpbGlwam9iZSIsImEiOiJjbGZ3dWhsdXcwYmJ0M2VxY2tyZjgzc3lkIn0.8BkhvRQElOx7NXiGvRBA-A';
 
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [lng, setLng] = useState(-99.9980711);
+  const [lat, setLat] = useState(48.3544091);
   const [zoom, setZoom] = useState(4);
 
   useEffect(() => {
