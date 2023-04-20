d8950ac62d1020df5694ef72e9559298b742623d
Philip <philipmjobe@gmail.com>
2023-04-20 14:53:28 -0400

    starts tweaking some css

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,45 +1,30 @@
 import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
-import L, { GeoJSON, Layer, LeafletMouseEvent, Marker } from 'leaflet';
-import statesData from './assets/us-states'
+import L, { GeoJSON, Layer, LeafletMouseEvent } from 'leaflet';
+import statesData from './assets/us-states';
 import { MapContainer } from 'react-leaflet';
-import  campground  from './Campgrounds'
-
 
 const LeafletMap = () => {
-  // const mapRef = useRef(null);
-  const [campgrounds, setCampgrounds] = useState<Campground[]>([]);
-
+  const mapRef = useRef(null);
 
   useEffect(() => {
-    const map = L.map('map').setView([48.3544091, -99.9980711], 4);
-
-    axios.get<Campground[]>("http://localhost:3000/campgrounds", {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(response => {
-      setCampgrounds(response.data);
-    })
-    .catch(ex => {
-      console.log(ex);
-    });
-  }, []);
-
+    const map = L.map('map', {
+      dragging: true,
+      scrollWheelZoom: true,
+    }).setView([48.3544091, -99.9980711], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
+      attribution: '© OpenStreetMap contributors',
     }).addTo(map);
 
     function style(feature: GeoJSON.Feature): L.PathOptions {
       return {
-        fillColor: 'black',
+        fillColor: 'grey',
         weight: 2,
         opacity: 1,
         color: 'white',
         dashArray: '3',
-        fillOpacity: 0.7
+        fillOpacity: 0.7,
       };
     }
 
@@ -50,43 +35,46 @@ const LeafletMap = () => {
         weight: 5,
         color: '#666',
         dashArray: '',
-        fillOpacity: 0.7
+        fillOpacity: 0.7,
       });
 
       layer.bringToFront();
     }
-
 
     function resetHighlight(e: LeafletMouseEvent) {
       geojson.resetStyle(e.target);
     }
 
     let geojson: GeoJSON;
+
     function zoomToFeature(e: LeafletMouseEvent) {
       map.fitBounds(e.target.getBounds());
     }
 
     function onEachFeature(feature: GeoJSON.Feature, layer: Layer) {
-      const campground = feature.properties;
-      const marker = L.marker([campground.lat, campground.lon]).addTo(map);
       layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        click: zoomToFeature
-    });
+        click: zoomToFeature,
+      });
     }
 
     geojson = L.geoJson(statesData, {
       style: style as L.StyleFunction,
-      onEachFeature: onEachFeature
+      onEachFeature: onEachFeature,
     }).addTo(map);
+
+    // Wait until the map is fully initialized before adding the drag handler
+    map.whenReady(() => {
+      map.dragging.enable();
+    });
 
     return () => {
       map.remove();
     };
   }, []);
 
-  return <MapContainer ref={mapRef} style={{ height: '100%', width: '100%' }} />;
+  return <MapContainer id='map' ref={mapRef} style={{ height: '100%', width: '100%', cursor: 'grab' }} />;
 };
-export default LeafletMap;
 
+export default LeafletMap;
