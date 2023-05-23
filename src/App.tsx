5e485a00042c9a7b77c8d0a7ba4396c80fbaea93
Philip <philipmjobe@gmail.com>
2023-05-23 12:11:07 -0400

    reverts to 2eabb6314c894a13fe4581e512790591769fc75a

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Campground } from './components/types';
@@ -5,12 +6,22 @@ import LeafletMap from './components/LeafletMap';
 
 const App: React.FC = () => {
   const [campgrounds, setCampgrounds] = useState<Campground[]>([]);
+=======
+import React, { useState, useEffect } from "react";
+import LeafletMap from "./components/LeafletMap";
+import axios from "axios";
 
+const App = () => {
+  // Define campground data
+  const [campgrounds, setCampgrounds] = useState([]);
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
+
+  // Fetch campground data from backend on component mount
   useEffect(() => {
     const fetchCampgrounds = async () => {
       try {
         const response = await axios.get("http://localhost:3000/campgrounds");
-        setCampgrounds(response.data);
+        setCampgrounds(response.data); // assuming response.data is an array of campground objects
       } catch (error) {
         console.error("Failed to fetch campgrounds:", error);
       }
@@ -19,8 +30,14 @@ const App: React.FC = () => {
   }, []);
 
   return (
+<<<<<<< HEAD
         <div style={{ height: '100vh', width: '100vw' }}>
       <LeafletMap campgrounds={campgrounds} />
+=======
+    <div id="map">
+      {/* Render Map component */}
+      <LeafletMap campgrounds={campgrounds}/>
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
     </div>
   );
 };
--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
+<<<<<<< HEAD
 import L, { GeoJSON, LeafletMouseEvent, Layer, PathOptions } from 'leaflet';
 import statesData from './assets/us-states';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { Campground } from './types';
 import { Feature, GeometryObject } from 'geojson';
+=======
+import L, { GeoJSON, Layer, LeafletMouseEvent } from 'leaflet';
+import statesData from './assets/us-states';
+import { MapContainer } from 'react-leaflet';
+import { Campground } from './types';
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
 
 interface CampgroundProps {
   campgrounds: Campground[];
 }
+<<<<<<< HEAD
 
 interface FeatureLayer extends Layer {
   feature?: Feature<GeometryObject> | null;
 }
 
+=======
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
 const myIcon = L.icon({
   iconUrl: 'components/assets/marker.png',
   iconSize: [38, 95],
@@ -21,13 +31,18 @@ const myIcon = L.icon({
   popupAnchor: [-3, -76],
   shadowUrl: 'my-icon-shadow.png',
   shadowSize: [68, 95],
-  shadowAnchor: [22, 94],
+  shadowAnchor: [22, 94]
 });
+<<<<<<< HEAD
 
 const LeafletMap: React.FC<CampgroundProps> = ({ campgrounds }: CampgroundProps) => {
   const geojsonRef = useRef<GeoJSON<GeometryObject, any> | null>(null);
   const [stateCampgrounds, setStateCampgrounds] = useState<Campground[]>([]);
   const [showMarkers, setShowMarkers] = useState<boolean>(false);
+=======
+const LeafletMap: React.FC<CampgroundProps> = ({campgrounds}: CampgroundProps) => {
+  const mapRef = useRef(null);
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
 
   useEffect(() => {
     function style(feature: Feature<GeometryObject>): PathOptions {
@@ -57,13 +72,20 @@ const LeafletMap: React.FC<CampgroundProps> = ({ campgrounds }: CampgroundProps)
     }
 
     function resetHighlight(e: LeafletMouseEvent) {
+<<<<<<< HEAD
       const geojson = geojsonRef.current;
       if (geojson) {
         geojson.resetStyle(e.target as FeatureLayer);
       }
+=======
+      geojson.resetStyle(e.target);
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
     }
 
+    let geojson: GeoJSON;
+
     function zoomToFeature(e: LeafletMouseEvent) {
+<<<<<<< HEAD
       const map = e.target?.getMap();
       if (map) {
         map.fitBounds(e.target.getBounds());
@@ -80,17 +102,41 @@ const LeafletMap: React.FC<CampgroundProps> = ({ campgrounds }: CampgroundProps)
       const featureLayer = layer as FeatureLayer;
 
       featureLayer.on({
+=======
+      map.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature: GeoJSON.Feature, layer: Layer) {
+      layer.on({
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
         mouseover: highlightFeature,
         mouseout: resetHighlight,
         click: zoomToFeature,
       });
+    
+      const stateName = feature.properties?.name;
+    
+      // Filter campgrounds for the current state
+      const stateCampgrounds = campgrounds.filter(campground => campground.state === stateName);
+    
+      stateCampgrounds.forEach(campground => {
+        console.log('Creating marker for campground', campground)
+        L.marker([campground.lat, campground.lng], { icon: myIcon }).addTo(map);
+      });
     }
+<<<<<<< HEAD
 
     const geojson = L.geoJSON(statesData as GeoJSON.GeoJsonObject, {
       style: style as L.PathOptions,
+=======
+    
+    geojson = L.geoJson(statesData, {
+      style: style as L.StyleFunction,
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
       onEachFeature: onEachFeature,
     });
 
+<<<<<<< HEAD
     geojsonRef.current = geojson;
 
     return () => {
@@ -131,3 +177,19 @@ const LeafletMap: React.FC<CampgroundProps> = ({ campgrounds }: CampgroundProps)
 };
 
 export default LeafletMap;
+=======
+    // Wait until the map is fully initialized before adding the drag handler
+    map.whenReady(() => {
+      map.dragging.enable();
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [campgrounds]);
+
+  return <MapContainer id='map' ref={mapRef} style={{ height: '100%', width: '100%', cursor: 'grab' }} />;
+};
+
+export default LeafletMap
+>>>>>>> parent of 2eabb63 (starts getting the markers actually working still debugging)
