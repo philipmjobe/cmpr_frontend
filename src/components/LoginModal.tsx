ac2a70fca45d3cec2ca2bd6469f55b7c68bc6386
Philip <philipmjobe@gmail.com>
2023-06-05 14:25:13 -0400

    working on modals for signing up and logging in

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -20,40 +20,6 @@ const Modal = ({ closeModal }: { closeModal: () => void }) => {
     }
   };
 
-  const handleSignup = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/users', {
-        first_name: '', // Add the necessary first name input value here
-        last_name: '', // Add the necessary last name input value here
-        email,
-        password,
-        password_confirmation: '', // Add the necessary password confirmation input value here
-      });
-  
-      // Handle the successful sign-up response
-      // You can customize this part based on your requirements
-      console.log(response.data); // You can access the response data here
-  
-      closeModal(); // Close the modal or perform any other necessary action
-    } catch (error: any) {
-      if (error.response) {
-        // Request made and server responded with an error status
-        console.log(error.response.data); // You can access the error response data here
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-  
-      setError('Error occurred during sign-up'); // Set an appropriate error message for the user
-    }
-  };
-  
-  
-
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10">
       <div className="bg-white rounded-lg shadow-lg p-6">
@@ -71,17 +37,11 @@ const Modal = ({ closeModal }: { closeModal: () => void }) => {
           />
           <button type="submit">Login</button>
         </form>
-        <h2 className="text-lg font-semibold mb-4">Sign Up</h2>
-        <form onSubmit={handleSignup}>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button type="submit">Sign Up</button>
-        </form>
         {error && <p>{error}</p>}
+        <button className="text-blue-500 hover:text-blue-700 underline" onClick={closeModal}>
+          Close
+        </button>
       </div>
-      <button className="text-blue-500 hover:text-blue-700 underline" onClick={closeModal}>
-        Close
-      </button>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './LoginModal';
 
 const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -9,9 +20,13 @@ const Navbar = () => {
             <span className="text-white font-semibold text-lg">Campr</span>
           </div>
           <div className="flex">
+            <button onClick={handleModalOpen} className="text-white hover:text-gray-200">
+              Login
+            </button>
           </div>
         </div>
       </div>
+      {isModalOpen && <Modal closeModal={handleModalClose} />}
     </nav>
   );
 };
